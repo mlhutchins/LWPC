ee41862210484027b89c82aefe90bd39c1263e23
Michael Hutchins <mlhutch@uw.edu>
2012-08-16 12:41:45 -0700

    Working on Mac OS 10.8. Adjusted write order for lwpcDAT.loc, cleaned up some unncessary files.

--- LWPCpar.m
+++ LWPCpar.m
@@ -15,8 +15,9 @@ end
 
 %% Get Worker ID and check for code
 
-lwpcProgram=sprintf('LWPC.%s',computer('arch'));
+arch=computer('arch');
 
+lwpcProgram=sprintf('LWPC.%s',arch);
 
 try
      worker=getCurrentTask;
@@ -27,15 +28,17 @@ catch
      worker=[];
 end
 
-
 folder=sprintf('lwpcpar%g',worker);
 path=pwd;
 
 if exist(sprintf('lwpcpar%g',worker),'dir')==0
     system(sprintf('cp -r lwpcpar %s',folder));
-    fid=fopen(sprintf('%s/lwpcDAT.loc',folder),'wt');
-    fprintf(fid,'%s/%s/lwpcv21/data/',path,folder); 
 end
+
+fid=fopen(sprintf('%s/lwpcDAT.loc',folder),'wt');
+% fprintf(fid,'%s/%s/lwpcv21.%s/data/',path,folder,arch); 
+fprintf(fid,'%s/%s/lwpcv21/data/',path,folder); 
+
 
 %% Write input file
 
@@ -81,31 +84,22 @@ else
 
 end
 
-% LWPM Homogeneous day
-% text={'test2','test2',freq,lat,-long,stat_lat,-stat_long};
-% fprintf(fid,'case-id %s\ntx %s\ntx-data lwpc %g %7.3f %7.3f 100 0 0 0\nionmatlabosphere homogeneous exponential 0.3 74\nreceivers %f %f\nlwflds\nprint-swg 0\nprint-mds 0\nprint-lwf 1\nlwf-vs-distance 20000 10000\nstart\nquit',text{1:7});
-% fclose(fid);
-
 %% Reset transmitter and model files
-
-system(sprintf('cp %s/lwpcv21/save/xmtr.lis %s/lwpcv21/data',folder,folder));
-
-% if isempty(strmatch('homog',model));
-system(sprintf('rm %s/%s/lwpc.mds',path,folder));
-% end
-% system('del C:\LWPCv21\save\delete\lwpc_cal.lwf');
-
-%% Run LWPC
-
-%fprintf('%s/%s/LWPC lwpc\n',path,folder);
 
 cd(sprintf('%s/%s',path,folder));
 
-system(sprintf('%s/%s/%s lwpc',path,folder,lwpcProgram));
+system(sprintf('cp lwpcv21.%s/save/xmtr.lis lwpcv21.%s/data',arch,arch));
+
+system('rm lwpc.mds');
+
+%% Run LWPC
+
+system(sprintf('./%s lwpc',lwpcProgram));
+
+% Lazy symbol binding: system(sprintf('./%s lwpc',lwpcProgram));
 
 cd(path)
 
-%eval(sprintf('cd %s',path));
 %% Read output log
 
 a=textread(sprintf('%s/lwpc.log',folder),'%s','headerlines',34); %open outputed log file
@@ -115,7 +109,4 @@ dist_lwpm=a{4};
 power_lwpm=str2double(power_lwpm);
 dist_lwpm=str2double(dist_lwpm);
 
-% cd ..
-
-% cd ~/Documents/MATLAB
 end
